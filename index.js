986044ffaf40d6e1a75433e326abac64bdd44ac8
joy-hui <tongwandou432@gmail.com>
2023-02-24 20:31:54 +0000

    updata questions

--- index.js
+++ index.js
@@ -5,11 +5,59 @@ const generateMarkdown = require("./utils/generateMarkdown");
 
 // array of questions for user
 const questions = [
-
+    {
+        type: "input",
+        name: "github",
+        message: "What is your GitHub username?",
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "What is your email address?",
+      },
+      {
+        type: "input",
+        name: "name",
+        message: "What is your project's name?",
+      },
+      {
+        type: "input",
+        name: "description",
+        message: "Please write a short description of your project ?",
+      },
+      {
+        type: "list",
+        name: "license",
+        message: "What is your GitHub username?",
+        choices: ["MIT", "APACHE 2.0", "GPL 3.0", "BSD 3", "None"],
+      },
+      {
+        type: "input",
+        name: "install",
+        message: "What command should be run to install dependencies?",
+        default: "npm i",
+      },
+      {
+        type: "input",
+        name: "test",
+        message: "What command should be run to run tests?",
+        default: "npm test",
+      },
+      {
+        type: "input",
+        name: "use",
+        message: "What does the user need to know about using the repo?",
+      },
+      {
+        type: "input",
+        name: "contribute",
+        message: "What does the user need to know about contributing to the repo?",
+      },
 ];
 
 // function to write README file
 function writeToFile(fileName, data) {
+    
 }
 
 // function to initialize program
