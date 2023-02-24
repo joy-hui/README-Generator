e21c8ad367f1d110c830f4485d996f04a79eb28a
joy-hui <tongwandou432@gmail.com>
2023-02-24 20:30:24 +0000

    updata the generateMarkdown

--- utils/  generateMarkdown.js
+++ utils/  generateMarkdown.js
@@ -1,7 +1,54 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
-    return `# ${data.title}
+    return `
+    # ${data.name}
+    ![GitHub license](https://img.shields.io/badge/license-${license}-blue.svg)
+    ## Description
+
+    ${data.description}
+
+    ## Table of Contents 
+
+    * [Installation](#installation)
+
+    * [Usage](#usage)
+
+    * [License](#License)
+
+    * [Contributing](#contributing)
+
+    * [Tests](#tests)
+
+    * [Questions](#questions)
+
+    ## Installation
+
+    The command should be run to install dependencies:
+
+    * ${data.install}
+
+    ## Usage
+
+    ${data.use}
+
+    ## License
+
+    ${data.license}
   
+    ## Contributing
+
+    ${data.contribute}
+
+    ## Tests
+
+    The command should be run to run tests:
+
+    * ${data.test}
+
+    ## Questions
+
+    If you have any questions please contact me, my email address is ${data.email}. 
+    This is my github: [${data.github}](https://github.com/${ data.github }/).
   `;
   }
   
